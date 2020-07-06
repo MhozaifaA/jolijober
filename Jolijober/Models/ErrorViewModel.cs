78ba3e8d15a4d982a2d3cdd55dcafbfe3631c167
MHozaifaA <botchey.2012@gmail.com>
2020-07-06 23:31:59 +0300

    test blazor  the convert to pure core asp for js and some marge  to template now !!

---
+++ Jolijober/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Jolijober.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
